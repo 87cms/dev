f019faa17e61eed650238c80c25f00e0a0dc44c3
unknown <devteam@87cms.com>
2013-12-18 16:30:47 +0100

    Fixes

--- config/constant.php
+++ config/constant.php
@@ -1,11 +1,1 @@
 <?php 
-define("_DOMAIN_", "hs.87cms.com");
-define("_COOKIE_DOMAIN_", "hs.87cms.com");
-define("SALT", '-0g^:XB|zixnP82IqQN)K:tVlcKsBcsj2m8');
-define("TMP", "tmp/"); // Deprecated
-define("_TMP_DIR_", "tmp/");
-define("_THEME_DIR_", _ABSOLUTE_PATH_.'/theme' );
-define("_MEDIAS_DIR_", _ABSOLUTE_PATH_.'/medias' );
-define("_ADMIN_DIR_", _ABSOLUTE_PATH_.'/admin' );
-define("_MODULES_DIR_", _ABSOLUTE_PATH_.'/modules' );
-define("_RICHTEXT_DIR_", _ADMIN_DIR_.'/jsrichtext/elements' );
--- config/db_settings.php
+++ config/db_settings.php
@@ -1,12 +1,1 @@
 <?php 
-/*define("DBHOST", 'localhost');
-define("DBNAME", 'hotelspider');
-define("DBUSER", 'hotelspider');
-define("DBPASSWORD", 'z3vaKnVMvwdtAOybnoVs');
-define("_DB_PREFIX_", '');*/
-
-define("DBHOST", 'mysql51-83.perso');
-define("DBNAME", 'cmsolduf879');
-define("DBUSER", 'cmsolduf879');
-define("DBPASSWORD", 'fdng6484dfnj');
-define("_DB_PREFIX_", '');
