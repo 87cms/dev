64e047c91ed5dbd9a777c3f596fa1e4b0c5bdc06
unknown <devteam@87cms.com>
2013-12-18 16:28:00 +0100

    Fixes

--- config/constant.php
+++ config/constant.php
@@ -1,11 +1,1 @@
 <?php 
-define("_DOMAIN_", "192.168.1.127");
-define("_COOKIE_DOMAIN_", "192.168.1.127");
-define("SALT", '-0g^:XB|zixnP82IqQN)K:tVlcKsBcsj2m8');
-define("TMP", "tmp/"); // Deprecated
-define("_TMP_DIR_", "tmp/");
-define("_THEME_DIR_", _ABSOLUTE_PATH_.'/theme' );
-define("_MEDIAS_DIR_", _ABSOLUTE_PATH_.'/medias' );
-define("_ADMIN_DIR_", _ABSOLUTE_PATH_.'/admin' );
-define("_MODULES_DIR_", _ABSOLUTE_PATH_.'/modules' );
-define("_RICHTEXT_DIR_", _ADMIN_DIR_.'/jsrichtext/elements' );
--- config/db_settings.php
+++ config/db_settings.php
@@ -1,6 +1,4 @@
 <?php 
-define("DBHOST", 'localhost');
-define("DBNAME", 'hotelspider');
-define("DBUSER", 'hotelspider');
-define("DBPASSWORD", 'z3vaKnVMvwdtAOybnoVs');
-define("_DB_PREFIX_", '');
+
+
+
